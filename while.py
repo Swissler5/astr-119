96cd8e5113d44d75e1856bebfd64f5df3e3880b9
Swissler5 <91558752+Swissler5@users.noreply.github.com>
2021-10-06 21:45:42 -0700

    Delete while.py

--- while.py
+++
@@ -1,7 +0,0 @@
-#Shea Wissler
-
-i = 0			#define integer and starting point
-
-while (i<119):		#sets the condition that i is less than 119
-	print(i)		#print the current
-	i += 10			#add 10 to the value of i
