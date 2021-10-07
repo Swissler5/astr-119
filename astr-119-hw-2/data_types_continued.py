40c3d18ad5efeef32a67777872060335ec35bc65
Swissler5 <91558752+Swissler5@users.noreply.github.com>
2021-10-06 21:50:14 -0700

    Add files via upload

---
+++ astr-119-hw-2/data_types_continued.py
@@ -0,0 +1,30 @@
+#Shea Wissler
+#string
+s = "I am a string."
+print(type(s))		#writes str
+
+#Boolean
+yes = True			#Boolean True
+print(type(yes))
+
+no = False			#Boolean False
+print(type(no))
+
+#List -- ordered and changeable
+
+alpha_list = ["a", "b", "c"]		#list initialization
+print(type(alpha_list)) 			#will say tuple
+print(type(alpha_list[0])) 			#will say string
+alpha_list.append("d")				#will add "d" to the list end
+print(alpha_list)					#will print list
+
+#Tuple -- ordered and unchangeable
+
+alpha_tuple = ("a", "b", "c") 	#tuple initialization
+print(type(alpha_tuple))		#will sat tuple
+
+try:							#attempt the following line
+	alpha_tuple[2] = "d"		#won't work and will raise TypeError
+except TypeError:				#esception for the Type Error
+	print("We can't ass elements to tuples!") 		#print the message
+print(alpha_tuple)			#will print tuple
---
+++ astr-119-hw-2/useful_modules.py
@@ -0,0 +1,9 @@
+#Shea Wissler
+import numpy as np 						#the Numpy library
+import matplotlib.pyplot as plt			#the Matploplib's library
+
+import sys								#gives access to C-like library
+import os								#gives access to operating system
+
+print(sys.argv)							#prints any command line arguments, incl program name
+print(os.getcwd())						#prints current working directory
